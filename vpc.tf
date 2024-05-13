223e5c28e5f448cd865e5621419c221778fc071c
Anncodes001 <annalexx2001@gmail.com>
2024-05-13 14:29:44 +0100

    renamed main.tf to vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -27,3 +27,32 @@ resource "aws_subnet" "private_subnets" {
    Name = "Private Subnet ${count.index + 1}"
  }
 }
+#Internet gateway
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.web_main.id
+
+  tags = {
+    Name = "igwmain"
+  }
+}
+
+# Second Route table (Public)
+resource "aws_route_table" "second_rt" {
+  vpc_id = aws_vpc.web_main.id
+  
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+  
+  tags = {
+    Name = "Second Route Table"
+  }
+}
+
+#route table association
+resource "aws_route_table_association" "public_subnet_asso" {
+  count = length(var.public_subnet_cidrs)
+  subnet_id      = aws_subnet.public_subnets[count.index].id
+  route_table_id = aws_route_table.second_rt.id
+}
