e7480e9332bd65c1e9ccde3c24c54b2b0fffa001
Anncodes001 <annalexx2001@gmail.com>
2024-05-13 11:48:24 +0100

    added code in provider and variable.tf fle stating the provider and configuring it

--- provider.tf
+++ provider.tf
@@ -1,1 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.49.0"
+    }
+  }
+}
 
+# Provider Configuration
+provider "aws" {
+  region = var.region
+}
+
+# Use data sources allow configuration to be
+# generic for any region
+data "aws_region" "current" {}
+data "aws_availability_zones" "available" {}
