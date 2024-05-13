3612898734d2e98dde809dc69741471f1393dd7c
Anncodes001 <annalexx2001@gmail.com>
2024-05-13 11:56:52 +0100

    added subnet cidrs in var.tf file and resource for vpc and subnet in main.tf file

---
+++ main.tf
@@ -0,0 +1,24 @@
+# VPC creation 
+resource "aws_vpc" "web_main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "public_subnets" {
+ count      = length(var.public_subnet_cidrs)
+ vpc_id     = aws_vpc.web_main.id
+ cidr_block = element(var.public_subnet_cidrs, count.index)
+ 
+ tags = {
+   Name = "Public Subnet ${count.index + 1}"
+ }
+}
+ 
+resource "aws_subnet" "private_subnets" {
+ count      = length(var.private_subnet_cidrs)
+ vpc_id     = aws_vpc.web_main.id
+ cidr_block = element(var.private_subnet_cidrs, count.index)
+ 
+ tags = {
+   Name = "Private Subnet ${count.index + 1}"
+ }
+}
--- variable.tf
+++ variable.tf
@@ -9,3 +9,15 @@ variable "number_of_public_subnets" {
 variable "number_of_private_subnets" {
   default = 2
 }
+
+variable "public_subnet_cidrs" {
+ type        = list(string)
+ description = "Public Subnet CIDR values"
+ default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+ 
+variable "private_subnet_cidrs" {
+ type        = list(string)
+ description = "Private Subnet CIDR values"
+ default     = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+}
