e07712ad6fff17fa6fc82a14c61cb3096e01c070
Anncodes001 <annalexx2001@gmail.com>
2024-05-13 14:56:35 +0100

    aws launch template and security group configurations added

--- vpc.tf
+++ vpc.tf
@@ -56,3 +56,55 @@ resource "aws_route_table_association" "public_subnet_asso" {
   subnet_id      = aws_subnet.public_subnets[count.index].id
   route_table_id = aws_route_table.second_rt.id
 }
+
+
+resource "aws_launch_template" "web_server_template" {
+  name_prefix             = "webServer"
+  image_id                = "ami-lt-0e223c5f0f93004fa" // Replace with your desired AMI
+  instance_type           = "t2.micro"
+  key_name                = var.key_pair_name // Specify your key pair name here
+  security_group_names    = [aws_security_group.web_server_sg.name]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              yum update -y
+              yum install -y httpd
+              systemctl start httpd
+              systemctl enable httpd
+              echo "<html><body><h1>Hello World!</h1></body></html>" > /var/www/html/index.html
+              EOF
+}
+
+resource "aws_security_group" "web_server_sg" {
+  name        = "web-server-sg"
+  description = "Security group for web server"
+
+  vpc_id      = aws_vpc.web_main.id 
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
