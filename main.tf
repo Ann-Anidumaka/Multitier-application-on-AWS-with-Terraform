8689c273bfb4f603e92ced9d02eae9021107abbe
Anncodes001 <annalexx2001@gmail.com>
2024-05-17 18:41:48 +0100

    added nat gateway

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
 # VPC creation 
 resource "aws_vpc" "web_main" {
   cidr_block = "10.0.0.0/16"
+  
 
   tags = {
     Name = "web-mainvpc"
@@ -13,6 +14,7 @@ resource "aws_subnet" "public_subnets" {
  vpc_id     = aws_vpc.web_main.id
  cidr_block = element(var.public_subnet_cidrs, count.index)
  availability_zone = element(var.azs, count.index)
+ map_public_ip_on_launch = true
  tags = {
    Name = "Public Subnet ${count.index + 1}"
  }
@@ -23,6 +25,7 @@ resource "aws_subnet" "private_subnets" {
  vpc_id     = aws_vpc.web_main.id
  cidr_block = element(var.private_subnet_cidrs, count.index)
  availability_zone = element(var.azs, count.index)
+ map_public_ip_on_launch = true
  tags = {
    Name = "Private Subnet ${count.index + 1}"
  }
@@ -59,13 +62,63 @@ resource "aws_route_table_association" "public_subnet_asso" {
   route_table_id = aws_route_table.second_rt.id
 }
 
+# Elastic IP for NAT Gateway
+resource "aws_eip" "nat" {
+  domain = "vpc"
+  tags = {
+    Name = "NAT Gateway EIP"
+  }
+}
+
+#Nat Gateway
+resource "aws_nat_gateway" "web_nat" {
+   allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public_subnets[0].id
+
+  tags = {
+    Name = "gw NAT"
+  }
+
+  # To ensure proper ordering, it is recommended to add an explicit dependency
+  # on the Internet Gateway for the VPC.
+  depends_on = [aws_internet_gateway.gw]
+}
+
+#Routetable
+
+# Creating a Route Table for the Nat Gateway!
+resource "aws_route_table" "web_nat_rt" {
+  depends_on = [
+    aws_nat_gateway.web_nat
+  ]
+
+  vpc_id = aws_vpc.web_main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.web_nat.id
+  }
+
+  tags = {
+    Name = "Route Table for NAT Gateway"
+  }
+
+}
+
+#Routetable association Nat gateway
+resource "aws_route_table_association" "private_subnet_asso" {
+  count = length(var.private_subnet_cidrs)
+  subnet_id      = aws_subnet.private_subnets[count.index].id
+  route_table_id = aws_route_table.web_nat_rt.id
+}
+
 
 resource "aws_launch_template" "web_server_template" {
   name_prefix             = "webServer"
   image_id                = "ami-07caf09b362be10b8" // Replace with your desired AMI
   instance_type           = "t2.micro"
   key_name                = "web-server" // Specify your key pair name here
-  security_group_names    = [aws_security_group.web_server_sg.name]
+  vpc_security_group_ids  = [aws_security_group.web_server_sg.id]
 
   user_data = filebase64("apache-install.sh")
 
@@ -88,6 +141,8 @@ resource "aws_autoscaling_group" "autoscale" {
     aws_subnet.public_subnets[0].id,
     aws_subnet.public_subnets[1].id
   ]
+
+  target_group_arns = [aws_lb_target_group.web_tg.arn]
 
   launch_template {
     id      = aws_launch_template.web_server_template.id
@@ -137,7 +192,7 @@ resource "aws_lb" "test" {
   security_groups    = [aws_security_group.web_server_sg.id]
   subnets            = aws_subnet.public_subnets[*].id
 
-  enable_deletion_protection = true
+  enable_deletion_protection = false
 
     tags = {
     Name = "webServer-alb"
